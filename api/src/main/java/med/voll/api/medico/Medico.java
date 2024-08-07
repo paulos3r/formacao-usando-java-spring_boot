95b1062199b03047272197457a259e7f904466a5
Paulo Sergio <036paulo@gmail.com>
2024-08-06 22:35:55 -0300

    Foi ajustado o projeto, criando as migrations de medicos, ajustando os campos com tratativas e validacoes da validations das colunas

--- api/src/main/java/med/voll/api/medico/Medico.java
+++ api/src/main/java/med/voll/api/medico/Medico.java
@@ -7,7 +7,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import med.voll.api.endereco.Endereco;
 
-@Table(name = "MEDICOS")
+@Table(name = "medicos")
 @Entity(name="Medico")
 @Getter
 @NoArgsConstructor
@@ -19,12 +19,21 @@ public class Medico {
   private Long id;
   private String nome;
   private String email;
+  private String telefone;
   private String crm;
 
   @Enumerated(EnumType.STRING)
   private Especialidade especialidade;
 
-  @Embedded  //considera que e arquivo diferente mais faz parte da mesma tabela de medicos precisamos tambem informar no arquivo de endereco a respeito
-  private Endereco Endereco;
+  @Embedded
+  private Endereco endereco;
 
+  public Medico( DadosCadastroMedico dados ) {
+    this.nome = dados.nome();
+    this.email = dados.email();
+    this.telefone = dados.telefone();
+    this.crm = dados.crm();
+    this.especialidade = dados.especialidade();
+    this.endereco = new Endereco( dados.endereco() );
+  }
 }
