5913c0f87462cd735dcdeca047dc3ad2bb292e91
Paulo Sergio <036paulo@gmail.com>
2024-08-10 22:57:21 -0300

    Foi ajustado o projeto para requisicao de atualizar e deletetar registros do banco de dados

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,10 +1,7 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosCadastroMedico;
-import med.voll.api.medico.DadosListagemMedico;
-import med.voll.api.medico.Medico;
-import med.voll.api.medico.MedicoRepository;
+import med.voll.api.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -29,7 +26,22 @@ public class MedicoController {
 
   @GetMapping
   public Page<DadosListagemMedico> listarMedico( @PageableDefault(size=10,page=0,sort={"nome"}) Pageable page){
-    return repository.findAll( page ).map(DadosListagemMedico::new);
+    //return repository.findAll( page ).map(DadosListagemMedico::new);
+    return repository.findAllByAtivoTrue( page ).map(DadosListagemMedico::new);
   }
 
+  @PutMapping
+  @Transactional
+  public void atualizarMedico(@RequestBody @Valid DadosAtualizacaoMedicos dados){
+    var medico = repository.getReferenceById(dados.id());
+    medico.atualizarInformacoes(dados);
+  }
+
+  @DeleteMapping("/{id}")
+  @Transactional
+  public void excluirMedico(@PathVariable Long id){
+    // repository.deleteById( id );
+    var medico = repository.getReferenceById(id);
+    medico.excluir();
+  }
 }
