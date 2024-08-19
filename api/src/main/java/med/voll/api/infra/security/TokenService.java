ae4f63c13c4ca6e6acd13808089157b1632abc2f
Paulo Sergio <036paulo@gmail.com>
2024-08-18 22:11:52 -0300

    Precisar subir so uma 1 parte do sistema de autenticacao

--- api/src/main/java/med/voll/api/infra/security/TokenService.java
+++ api/src/main/java/med/voll/api/infra/security/TokenService.java
@@ -3,6 +3,7 @@ package med.voll.api.infra.security;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import med.voll.api.domain.usuario.Usuario;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -28,6 +29,23 @@ public class TokenService {
     } catch (JWTCreationException exception){
       // Invalid Signing configuration / Couldn't convert Claims.
       throw new RuntimeException("Erro ao gerar token jwt", exception);
+    }
+  }
+
+  public String getSubject(String tokenJWT){
+    try {
+      var algorithm = Algorithm.HMAC256(secret);
+      return JWT.require(algorithm)
+              // specify any specific claim validations
+              .withIssuer("API Voll.mad")
+              // reusable verifier instance
+              .build()
+              .verify(tokenJWT)
+              .getSubject();
+
+    } catch (JWTVerificationException exception){
+      // Invalid signature/claims
+      throw new RuntimeException("Token jwt inválido ou expirado!");
     }
   }
 
